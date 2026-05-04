ad7e1a04489c19cb2580bfdd41d365aa335d9ba9
nnsquik <nnsquik@outlook.com>
2026-05-04 16:09:21 +0300

    project initialization

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-### Hexlet tests and linter status:
 [![Actions Status](https://github.com/nnsquik/java-project-72/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/nnsquik/java-project-72/actions)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=nnsquik_java-project-72&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=nnsquik_java-project-72)
---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("java")
+    id("org.sonarqube") version "7.2.3.7755"
+    checkstyle
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "nnsquik_java-project-72")
+        property("sonar.organization", "nesquik")
+    }
+}
+
+checkstyle {
+    configFile = file("config/checkstyle/checkstyle.xml")
+    configProperties = mapOf(
+        "org.checkstyle.sun.suppressionfilter.config" to "${rootDir}/config/checkstyle/checkstyle-suppressions.xml"
+    )
+}
